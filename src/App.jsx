f942a82acb433a676c05ce1929a95612574ec8a3
HarshitRajput0201 <harshitrajput0201@gmail.com>
2024-12-04 19:32:12 +0530

    Tailwind Configured

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css';
+import Home from "./pages/Home.jsx";
+import { Route, Routes } from 'react-router-dom';
+
 
 function App() {
-
   return (
-    <div>
-      Hello
+    <div className=''>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
